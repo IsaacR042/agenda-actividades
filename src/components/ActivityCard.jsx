e3f164b24ee4947284b716ca8f8820292ea71b81
IsaacR042 <rodriguezisaac042@gmail.com>
2026-08-10 02:37:13 -0600

    feat: crear estructura inicial de la agenda

---
+++ src/components/ActivityCard.jsx
@@ -0,0 +1,27 @@
+function ActivityCard() {
+  return (
+    <article>
+      <h3>Ejemplo de actividad</h3>
+
+      <p>Fecha de entrega: 20/08/2026</p>
+
+      <p>
+        Esta es una actividad de ejemplo.
+      </p>
+
+      <button>
+        Completar
+      </button>
+
+      <button>
+        Editar
+      </button>
+
+      <button>
+        Eliminar
+      </button>
+    </article>
+  );
+}
+
+export default ActivityCard;
